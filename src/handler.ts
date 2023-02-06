40b1440393717d0656f3d723c13116583f556b09
Nathan Brown <nathan.brown@bjss.com>
2023-02-06 09:25:05 +0000

    chore(cb2-7355): lint

--- src/handler.ts
+++ src/handler.ts
@@ -1,13 +1,15 @@
 import 'source-map-support/register';
 import logger from './observability/logger';
 
-const { NODE_ENV, SERVICE, AWS_PROVIDER_REGION, AWS_PROVIDER_STAGE } = process.env;
+const {
+  NODE_ENV, SERVICE, AWS_PROVIDER_REGION, AWS_PROVIDER_STAGE,
+} = process.env;
 
 logger.info(
   `\nRunning Service:\n '${SERVICE}'\n mode: ${NODE_ENV}\n stage: '${AWS_PROVIDER_STAGE}'\n region: '${AWS_PROVIDER_REGION}'\n\n`,
 );
 
-const handler = async (): void => {
+const handler = (): void => {
   logger.debug("Function triggered'.");
 };
 
