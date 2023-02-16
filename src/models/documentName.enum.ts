77c5bd10d75df309f3a5aa8b136dfe9fc6648ac0
Nathan Brown <nathan.brown@bjss.com>
2023-02-16 14:59:39 +0000

    feat(cb2-7048): update to allow trl service letter

--- src/models/documentName.enum.ts
+++ src/models/documentName.enum.ts
@@ -1,4 +1,5 @@
 export const enum DocumentType {
   MINISTRY = 'VTG6_VTG7',
   MINISTRY_TRL = 'VTG6_VTG7_TRL',
+  TRL_INTO_SERVICE = 'TRAILER_INTO_SERVICE',
 }
---
+++ src/models/trlServiceLetter.ts
@@ -0,0 +1,10 @@
+import { IApplicantDetails } from './vehicleTechRecord';
+
+export type TrlServiceLetter = {
+  Vin: string;
+  TrailerID: string;
+  ApplicantDetails: IApplicantDetails;
+  LetterDateRequested: string;
+  TypeApprovalNumber: string;
+  ParagraphID: number;
+};
