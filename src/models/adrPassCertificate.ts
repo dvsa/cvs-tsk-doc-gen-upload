5a48418d85eef9885807e27194ebab01915c6e7b
naathanbrown <nathancjb@googlemail.com>
2024-01-29 15:27:13 +0000

    feat(cb2-10457): add in new required data

--- src/models/adrPassCertificate.ts
+++ src/models/adrPassCertificate.ts
@@ -1,7 +1,7 @@
 import { ADRCertificateTypes } from '@dvsa/cvs-type-definitions/types/v3/tech-record/enums/adrCertificateTypes.enum.js';
 import { DocumentName } from '../enums/documentName.enum';
 import { DocumentModel } from './documentModel';
-import { Request } from './request';
+import { HgvTrlLgv, Request } from './request';
 
 export type AdrCert = {
   vin: string;
@@ -28,11 +28,13 @@ export type AdrCert = {
   specialProvisions: string;
   tankStatement: {
     substancesPermitted: string;
-    statements: string;
+    statement: string;
     productList: string;
   };
   notes: string;
   replacement: boolean;
+  batteryListNumber: string;
+  m145Statement: boolean;
 };
 
 export class AdrPassCertificateDocument extends DocumentModel {
@@ -62,7 +64,7 @@ export class AdrPassCertificateDocument extends DocumentModel {
       adrVehicleType: techRecord.techRecord_adrDetails_vehicleDetails_type,
       permittedDangerousGoods: techRecord.techRecord_adrDetails_permittedDangerousGoods,
       brakeEndurance: techRecord.techRecord_adrDetails_brakeEndurance,
-      weight: techRecord.techRecord_adrDetails_weight,
+      weight: techRecord.techRecord_adrDetails_weight?.toString(),
       tankManufacturer: techRecord.techRecord_adrDetails_tank_tankDetails_tankManufacturer,
       tankManufactureSerialNo: techRecord.techRecord_adrDetails_tank_tankDetails_tankManufacturerSerialNo,
       tc2InitApprovalNo: techRecord.techRecord_adrDetails_tank_tankDetails_tc2Details_tc2IntermediateApprovalNo,
@@ -71,15 +73,31 @@ export class AdrPassCertificateDocument extends DocumentModel {
       specialProvisions: techRecord.techRecord_adrDetails_tank_tankDetails_specialProvisions,
       tankStatement: {
         substancesPermitted: techRecord.techRecord_adrDetails_tank_tankDetails_tankStatement_substancesPermitted,
-        statements: techRecord.techRecord_adrDetails_tank_tankDetails_tankStatement_statement,
-        productList: techRecord.techRecord_adrDetails_tank_tankDetails_tankStatement_productList,
+        statement: techRecord.techRecord_adrDetails_tank_tankDetails_tankStatement_statement,
+        productList: this.formatProductList(techRecord),
       },
       notes: techRecord.techRecord_adrDetails_adrCertificateNotes,
       replacement: request.adrCertificate.certificateType === ADRCertificateTypes.REPLACEMENT,
+      batteryListNumber: techRecord.techRecord_adrDetails_batteryListNumber,
+      m145Statement: techRecord.techRecord_adrDetails_m145Statement,
     };
 
     this.ADR_DATA = adrData;
   }
+
+  formatProductList = (techRecord: HgvTrlLgv) => {
+    let productList = '';
+    if (techRecord.techRecord_adrDetails_tank_tankDetails_tankStatement_productListUnNo) {
+      techRecord.techRecord_adrDetails_tank_tankDetails_tankStatement_productListUnNo.forEach(
+        // eslint-disable-next-line no-return-assign
+        (unNumber) => (productList += `${unNumber} `),
+      );
+    }
+    if (techRecord.techRecord_adrDetails_tank_tankDetails_tankStatement_productListRefNo) {
+      productList += `${techRecord.techRecord_adrDetails_tank_tankDetails_tankStatement_productListRefNo} `;
+    }
+    return productList;
+  };
 
   ADR_DATA: AdrCert;
 }
